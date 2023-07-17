8abbceb0dc8d58b1a03c143461f7050871c87e31
Budi Santoso <budi1173@gmail.com>
2023-07-17 09:08:39 +0700

    mengganti bola.dart,pemukul.dar, dan pong versi 1

--- lib/pong.dart
+++ lib/pong.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:game_pong/bola.dart';
+import 'package:game_pong/pemukul.dart';
 
 class Pong extends StatefulWidget {
   const Pong({Key? key}) : super(key: key);
@@ -8,12 +10,32 @@ class Pong extends StatefulWidget {
 }
 
 class _PongState extends State<Pong> {
+  double lebarScreen = 200;
+  double tinggiScreen = 500;
+  double diameter = 50;
+  double panjangPemukul = 100;
+  double lebarPemukul = 25;
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraint) {
+      lebarScreen = constraint.maxWidth;
+      tinggiScreen = constraint.maxHeight;
+      diameter = lebarScreen / 10;
+      panjangPemukul = lebarScreen / 5;
+      lebarPemukul = tinggiScreen / 20;
       return Stack(
         children: [
-          Text('Ini komponen pertama'),
+          Positioned(
+            top: 10,
+            child: Bola(diameter: diameter),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Pemukul(
+              panjang: panjangPemukul,
+              lebar: lebarPemukul,
+            ),
+          ),
         ],
       );
     });
